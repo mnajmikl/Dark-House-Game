7179fd20f58ed7bea9aca8c1d853b6c354d53c2c
Najmi Bachok <najmibachok@gmail.com>
2019-11-09 23:49:41 +0800

    New Game class

---
+++ DarkHouse/Games/Game.Functionality.cs
@@ -0,0 +1,29 @@
+﻿/* 
+ * DarkHouse
+ * Horror Exploration Game
+ * Copyright (c) 2019 Mohammad Najmi
+ */
+
+namespace DarkHouse.Games
+{
+    public partial class Game
+    {
+        /// <summary>
+        /// Create game session
+        /// </summary>
+        /// <remarks>Use this method to create a game session</remarks>
+        public void CreateSession()
+        {
+
+        }
+
+        /// <summary>
+        /// End game session
+        /// </summary>
+        /// <remarks>Use this method to end a game session</remarks>
+        public void EndGame()
+        {
+
+        }
+    }
+}
---
+++ DarkHouse/Games/Game.cs
@@ -0,0 +1,104 @@
+﻿/* 
+ * DarkHouse
+ * Horror Exploration Game
+ * Copyright (c) 2019 Mohammad Najmi
+ */
+
+using DarkHouse.House;
+using DarkHouse.Characters;
+
+namespace DarkHouse.Games
+{
+    /// <summary>
+    /// Class Game
+    /// </summary>
+    /// <remarks>Defines a Game</remarks>
+    public partial class Game
+    {
+        // private members of Game
+        private IHouse __house;
+        private ICharacter[] __players;
+        private string __name;
+        private string __title;
+        private string __description;
+
+        /// <summary>
+        /// Game constructor
+        /// </summary>
+        /// <param name="name">Game name</param>
+        /// <param name="title">Game title</param>
+        /// <param name="description">Game description</param>
+        /// <param name="house">Game house object</param>
+        /// <param name="players">Game characters objects</param>
+        /// <remarks>Defines a new Game</remarks>
+        public Game(string name, string title, string description, IHouse house, ICharacter[] players)
+        {
+            __name = name;
+            __title = title;
+            __description = description;
+
+            if (house != null)
+                __house = house;
+
+            if (players != null && players.Length > 0)
+                players.CopyTo(__players, players.Length - 1);
+            else
+            {
+                // Default to 10 characters
+                __players = new ICharacter[10];
+                // First character is a player
+                //__players[1] = new Player();
+                // Three Guides
+                //__player[2] = new Guide();
+                // Six Spooks
+                // __player[5] = new Spook();
+            }
+                
+        }
+
+        /// <summary>
+        /// House of current Game
+        /// </summary>
+        /// <returns><see cref="IHouse" /></returns>
+        /// <remarks>Returns the current game house</remarks>
+        public IHouse House { get => __house; }
+
+        /// <summary>
+        /// Characters of current Game
+        /// </summary>
+        /// <returns>Array of <see cref="ICharacter" /></returns>
+        /// <remarks>Returns the current game characters</remarks>
+        public ICharacter[] Players { get => __players; }
+
+        /// <summary>
+        /// Name of current Game
+        /// </summary>
+        /// <returns><see cref="string" /></returns>
+        /// <remarks>Returns the current game name</remarks>
+        public string Name { get => __name; }
+
+        /// <summary>
+        /// Title of current Game
+        /// </summary>
+        /// <returns><see cref="string" /></returns>
+        /// <remarks>Get or set the current game name</remarks>
+        public string Title { get => __title; set => __title = value; }
+
+        /// <summary>
+        /// Description of current Game
+        /// </summary>
+        /// <returns><see cref="string" /></returns>
+        /// <remarks>Get or set the current description name</remarks>
+        public string Description { get => __description; set => __description = value; }
+
+        /// <summary>
+        /// Get the hash code of current Game
+        /// </summary>
+        /// <returns><see cref="int" /></returns>
+        /// <remarks>Returns the current game hash code</remarks>
+        public override int GetHashCode()
+        {
+            return this.GetHashCode() & __name.GetHashCode() & __title.GetHashCode() & __description.GetHashCode();
+        }
+    }
+}
