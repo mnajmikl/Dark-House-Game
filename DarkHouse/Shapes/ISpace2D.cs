6818a1161711c2f6a9ce4f23678a8ab972100e0a
Najmi Bachok <najmibachok@gmail.com>
2019-11-09 17:24:39 +0800

    Added new ISpace derived interfaces

---
+++ DarkHouse/Shapes/ISpace2D.cs
@@ -0,0 +1,16 @@
+﻿/* 
+ * DarkHouse
+ * Horror Exploration Game
+ * Copyright (c) 2019 Mohammad Najmi
+ */
+
+namespace DarkHouse.Shapes
+{
+    /// <summary>
+    /// ISpace3D Interface
+    /// </summary>
+    /// <remarks>Defines a 3D space</remarks>
+    public interface ISpace3D : ISpace, IShape3D
+    {
+    }
+}
---
+++ DarkHouse/Shapes/ISpace3D.cs
@@ -0,0 +1,16 @@
+﻿/* 
+ * DarkHouse
+ * Horror Exploration Game
+ * Copyright (c) 2019 Mohammad Najmi
+ */
+
+namespace DarkHouse.Shapes
+{
+    /// <summary>
+    /// ISpace2D Interface
+    /// </summary>
+    /// <remarks>Defines a 2D space</remarks>
+    public interface ISpace2D : ISpace, IShape2D
+    {
+    }
+}
