fb103f88ba8ae5f67fae5701c610d0130050ccd9
Najmi Bachok <najmibachok@gmail.com>
2019-11-09 14:51:18 +0800

    Added inventory

---
+++ DarkHouse/Inventory/IInventory.cs
@@ -0,0 +1,43 @@
+﻿/* 
+ * DarkHouse
+ * Horror Exploration Game
+ * Copyright (c) 2019 Mohammad Najmi
+ */
+
+namespace DarkHouse.Inventory
+{
+    /// <summary>
+    /// IInventory Interface
+    /// </summary>
+    /// <remarks>Defines area of an inventory</remarks>
+    public interface IInventory
+    {
+        /// <summary>
+        /// Inventory name
+        /// </summary>
+        /// <returns><see cref="string"/></returns>
+        /// <remarks>Get or set the inventory name</remarks>
+        string Name { get; set; }
+
+        /// <summary>
+        /// Inventory tag
+        /// </summary>
+        /// <returns><see cref="string"/></returns>
+        /// <remarks>Get or set the inventory tag</remarks>
+        string Tag { get; set; }
+
+        /// <summary>
+        /// Inventory description
+        /// </summary>
+        /// <returns><see cref="string"/></returns>
+        /// <remarks>Get or set the inventory tag</remarks>
+        string Description { get; set; }
+
+        /// <summary>
+        /// Inventory items
+        /// </summary>
+        /// <returns><see cref="IItem"/> array</returns>
+        /// <remarks>Get or set the inventory items</remarks>
+        IItem[] Items { get;  set; }
+    }
+}
