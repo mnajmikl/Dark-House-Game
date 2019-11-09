28d5cee0dd57b1ef4d14d0170be8020c19e4dcbb
Najmi Bachok <najmibachok@gmail.com>
2019-11-09 17:30:22 +0800

    Added IWindow interface

---
+++ DarkHouse/House/IWindow.cs
@@ -0,0 +1,15 @@
+﻿/* 
+ * DarkHouse
+ * Horror Exploration Game
+ * Copyright (c) 2019 Mohammad Najmi
+ */
+
+using DarkHouse.Behaviors;
+using DarkHouse.Shapes;
+
+namespace DarkHouse.House
+{
+    interface IWindow : IHousePart, ILockable, IOpenable, IShape3D
+    {
+    }
+}
