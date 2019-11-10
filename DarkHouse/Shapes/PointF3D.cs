1ecd53852ecb8885a82035748043c49ea8566cdd
Najmi Bachok <najmibachok@gmail.com>
2019-11-11 01:07:13 +0800

    Added PointF3D and Side3D

---
+++ DarkHouse/Shapes/PointF3D.cs
@@ -0,0 +1,115 @@
+﻿/* 
+ * DarkHouse
+ * Horror Exploration Game
+ * Copyright (c) 2019 Mohammad Najmi
+ */
+
+using System;
+
+namespace DarkHouse.Shapes
+{
+    /// <summary>
+    /// PointF3D Struct
+    /// </summary>
+    /// <remarks>Defines a 3D point in float values</remarks>
+    public struct PointF3D
+    {
+        // private members of PointF3D
+        private readonly float _x;
+        private readonly float _y;
+        private readonly float _z;
+
+        /// <summary>
+        /// Constructor of a PointF3D
+        /// </summary>
+        /// <param name="x">X axis point</param>
+        /// <param name="y">Y axis point</param>
+        /// <param name="z">Z axis point</param>
+        /// <remarks>Defines a new PointF3D with a given X, Y, and Z points</remarks>
+        public PointF3D(float x, float y, float z)
+        {
+            if (x < 0.0f)
+            {
+                throw new ArgumentOutOfRangeException("x", "Value of x must be larger than 0.0");
+            }
+            else if (y < 0.0f)
+            {
+                throw new ArgumentOutOfRangeException("y", "Value of y must be larger than 0.0");
+            }
+            else if (z < 0.0f)
+            {
+                throw new ArgumentOutOfRangeException("z", "Value of z must be larger than 0.0");
+            }
+            else
+            {
+                _x = x;
+                _y = y;
+                _z = z;
+            }
+        }
+
+        /// <summary>
+        /// X axis point for a PointF3D
+        /// </summary>
+        /// <returns><see cref="float"/></returns>
+        /// <remarks>Returns the X axis point</remarks>
+        public float X { get => _x; }
+
+        /// <summary>
+        /// Y axis point for a PointF3D
+        /// </summary>
+        /// <returns><see cref="float"/></returns>
+        /// <remarks>Returns the Y axis point</remarks>
+        public float Y { get => _y; }
+
+        /// <summary>
+        /// Z axis point for a PointF3D
+        /// </summary>
+        /// <returns><see cref="float"/></returns>
+        /// <remarks>Returns the Z axis point</remarks>
+        public float Z { get => _z; }
+
+        /// <summary>
+        /// Overrides base Object Equals() method
+        /// </summary>
+        /// <returns><see cref="bool"/></returns>
+        /// <remarks>Returns true if the compared object is a <see cref="PointF3D"/> and all the X/Y/Z points are equal</remarks>
+        public override bool Equals(object obj)
+        {
+            if ((obj is PointF3D o))
+                return (this.X == o.X && this.Y == o.Y && this.Z == o.Z);
+            else
+                throw new ArgumentException("obj must be typeof PointF3D");
+        }
+
+        /// <summary>
+        /// Overrides base Object GetHashCode() method
+        /// </summary>
+        /// <returns><see cref="int"/></returns>
+        /// <remarks>Returns the AND values for the object and its X/Y/Z GetHashCode()</remarks>
+        public override int GetHashCode()
+        {
+            return this.GetHashCode() & X.GetHashCode() & Y.GetHashCode() & Z.GetHashCode();
+        }
+
+        /// <summary>
+        /// Operator == for <see cref="PointF3D"/>
+        /// </summary>
+        /// <returns><see cref="bool"/></returns>
+        /// <remarks>Returns true if the compared <see cref="PointF3D"/> has all equal X/Y/Z point values</remarks>
+        public static bool operator ==(PointF3D left, PointF3D right)
+        {
+            return left.X == right.X && left.Y == right.Y && left.Z == right.Z;
+        }
+
+        /// <summary>
+        /// Operator != for <see cref="PointF3D"/>
+        /// </summary>
+        /// <returns><see cref="bool"/></returns>
+        /// <remarks>Returns true if the compared <see cref="PointF3D"/> object has at least one unequal value for their X/Y/Z points</remarks>
+        public static bool operator !=(PointF3D left, PointF3D right)
+        {
+            return left.X != right.X || left.Y != right.Y || left.Z != right.Z;
+        }
+    }
+}
---
+++ DarkHouse/Shapes/Side3D.cs
@@ -0,0 +1,127 @@
+﻿/* 
+ * DarkHouse
+ * Horror Exploration Game
+ * Copyright (c) 2019 Mohammad Najmi
+ */
+
+using System;
+using System.Drawing;
+
+namespace DarkHouse.Shapes
+{
+    /// <summary>
+    /// Side struct
+    /// </summary>
+    /// <remarks>Defines side of a shape</remarks>
+    public struct Side3D
+    {
+        // private members for Side struct
+        private PointF3D __pointA;
+        private PointF3D __pointB;
+
+        /// <summary>
+        /// Constructor of a Side3D
+        /// </summary>
+        /// <param name="pointA">First side point</param>
+        /// <param name="pointB">Second side point</param>
+        /// <remarks>Defines a new side with a given length</remarks>
+        public Side3D(PointF3D pointA, PointF3D pointB)
+        {
+            if (!pointA.Equals(pointB))
+            {
+                if (pointA.X < 0.0f)
+                    throw new ArgumentOutOfRangeException("pointA.X", "Value of pointA.X must be larger than 0.0");
+                else if (pointA.Y < 0.0f)
+                    throw new ArgumentOutOfRangeException("pointA.Y", "Value of pointA.Y must be larger than 0.0");
+                else if (pointA.Z < 0.0f)
+                    throw new ArgumentOutOfRangeException("pointA.Z", "Value of pointA.Z must be larger than 0.0");
+                else if (pointB.X < 0.0f)
+                    throw new ArgumentOutOfRangeException("pointB.X", "Value of pointB.X must be larger than 0.0");
+                else if (pointB.Y < 0.0f)
+                    throw new ArgumentOutOfRangeException("pointB.Y", "Value of pointB.Y must be larger than 0.0");
+                else if (pointB.Z < 0.0f)
+                    throw new ArgumentOutOfRangeException("pointB.Z", "Value of pointB.Z must be larger than 0.0");
+                else
+                {
+                    __pointA = pointA;
+                    __pointB = pointB;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Coordinate points pointA and pointB must not have the same values");
+            }
+        }
+
+
+        /// <summary>
+        /// Distance or length of a <see cref="Side3D"/>
+        /// </summary>
+        /// <returns><see cref="float"/></returns>
+        /// <remarks>Returns the distance or length of a <see cref="Side3D"/></remarks>
+        public float Length()
+        {
+            float distanceX = MathF.Pow(__pointA.X - __pointB.X, 2.0f);
+            float distanceY = MathF.Pow(__pointA.Y - __pointB.Y, 2.0f);
+            float distanceZ = MathF.Pow(__pointA.Z - __pointB.Z, 2.0f);
+            return MathF.Sqrt(distanceX + distanceY + distanceZ);
+        }
+
+        /// <summary>
+        /// First point of a side
+        /// </summary>
+        /// <returns><see cref="PointF"/></returns>
+        /// <remarks>Returns the first point of a side for 3D shape</remarks>
+        public PointF3D PointA { get => __pointA; }
+
+        /// <summary>
+        /// Second point of a side
+        /// </summary>
+        /// <returns><see cref="PointF"/></returns>
+        /// <remarks>Returns the second point of a side for 3D shape</remarks>
+        public PointF3D PointB { get => __pointB; }
+
+        /// <summary>
+        /// Overrides base Object Equals() method
+        /// </summary>
+        /// <returns><see cref="bool"/></returns>
+        /// <remarks>Returns true if the compared object is a <see cref="Side"/> and all the PointA and PointB are equal</remarks>
+        public override bool Equals(object obj)
+        {
+            if (obj is Side3D s)
+                return (__pointA == s.PointA && __pointB == s.PointB);
+            else
+                throw new ArgumentException("obj must be typeof Side");
+        }
+
+        /// <summary>
+        /// Overrides base Object GetHashCode() method
+        /// </summary>
+        /// <returns><see cref="int"/></returns>
+        /// <remarks>Returns the AND values for the object and its __pointA and __pointB GetHashCode()</remarks>
+        public override int GetHashCode()
+        {
+            return GetHashCode() & __pointA.GetHashCode() & __pointB.GetHashCode();
+        }
+
+        /// <summary>
+        /// Operator == for <see cref="Side"/>
+        /// </summary>
+        /// <returns><see cref="bool"/></returns>
+        /// <remarks>Returns true if the compared <see cref="Side"/> has all equal PointA and PointB values</remarks>
+        public static bool operator ==(Side3D left, Side3D right)
+        {
+            return left.PointA == right.PointA && left.PointB == left.PointB;
+        }
+
+        /// <summary>
+        /// Operator != for <see cref="Side"/>
+        /// </summary>
+        /// <returns><see cref="bool"/></returns>
+        /// <remarks>Returns true if the compared <see cref="Side"/> object has at least one unequal value for their PointA and PointB values</remarks>
+        public static bool operator !=(Side3D left, Side3D right)
+        {
+            return left.PointA != right.PointA || left.PointB != left.PointB;
+        }
+    }
+}
