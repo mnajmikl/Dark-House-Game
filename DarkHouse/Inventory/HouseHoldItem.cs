7a43394d0e859bdac1fa901c6e1379dc6fc20b3f
mnajmikl <mnajmi@gmail.com>
2019-11-13 14:03:38 +0800

    Updated HouseHoldItem class

--- DarkHouse/Inventory/HouseHoldItem.cs
+++ DarkHouse/Inventory/HouseHoldItem.cs
@@ -1,7 +1,30 @@
 namespace DarkHouse.Inventory
 {
+    /// <summary>
+    /// HouseHoldItem Class
+    /// </summary>
+    /// <remarks>Define a house hold item</remarks>
     public class HouseHoldItem : IItem
     {
-        
+               /// <summary>
+        /// Item name
+        /// </summary>
+        /// <returns><see cref="string"/></returns>
+        /// <remarks>Get or set the item name</remarks>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Item tag
+        /// </summary>
+        /// <returns><see cref="string"/></returns>
+        /// <remarks>Get or set the item tag</remarks>
+        public string Tag { get; set; }
+
+        /// <summary>
+        /// Item description
+        /// </summary>
+        /// <returns><see cref="string"/></returns>
+        /// <remarks>Get or set the item tag</remarks>
+        public string Description { get; set; }
     }
 }
