e785088cb638249c512e18e5b0fa317a10538ee1
mnajmikl <mnajmi@gmail.com>
2019-11-15 12:11:51 +0800

    Relocated HouseHoldItem to a new sub namespace

---
+++ DarkHouse/Inventory/HouseHoldItem/HouseHoldItem.cs
@@ -0,0 +1,69 @@
+using DarkHouse.Inventory;
+
+namespace DarkHouse.Inventory.HouseHoldItem
+{
+    /// <summary>
+    /// HouseHoldItem Class
+    /// </summary>
+    /// <remarks>Define a house hold item</remarks>
+    public abstract class HouseHoldItem : IItem
+    {
+        private string __name;
+        private string __tag;
+        private string __description;
+        /// <summary>
+        /// HouseHoldItem constructor
+        /// </summary>
+        /// <remarks>Defines a new HouseHoldItem</remarks>
+        public HouseHoldItem(string name, string tag, string description)
+        {
+            __name = name;
+            __tag = tag;
+            __description = description;
+        }
+        /// <summary>
+        /// HouseHoldItem name
+        /// </summary>
+        /// <returns><see cref="string"/></returns>
+        /// <remarks>Get or set the household item name</remarks>
+        public string Name { get => __name; set => __name = value; }
+
+        /// <summary>
+        /// HouseHoldItem tag
+        /// </summary>
+        /// <returns><see cref="string"/></returns>
+        /// <remarks>Get or set the household item tag</remarks>
+        public string Tag { get => __tag; set => __tag = value; }
+
+        /// <summary>
+        /// HouseHoldItem description
+        /// </summary>
+        /// <returns><see cref="string"/></returns>
+        /// <remarks>Get or set the household item tag</remarks>
+        public string Description { get => __description; set => __description = value; }
+
+        /// <summary>
+        /// Pick up a household item
+        /// </summary>
+        /// <remarks>Defines how a household item is picked up by a player</remarks>
+        public abstract void PickUp();
+
+        /// <summary>
+        /// Drop a household item
+        /// </summary>
+        /// <remarks>Defines how a household item is dropped by a player</remarks>
+        public abstract void Drop();
+ 
+        /// <summary>
+        /// Inspect a household item
+        /// </summary>
+        /// <remarks>Defines how a household item is inspected by a player</remarks>
+        public abstract void Inspect();
+
+        /// <summary>
+        /// Use a household item
+        /// </summary>
+        /// <remarks>Defines how a household item is used by a player</remarks>
+        public abstract void Use();
+    }
+}
