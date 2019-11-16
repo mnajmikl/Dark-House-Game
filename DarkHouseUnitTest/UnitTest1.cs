ac77fc0c343f8d9487d39d9e8736161c4890acad
Najmi Bachok <najmibachok@gmail.com>
2019-11-16 10:17:49 +0800

    Added a test unit

---
+++ DarkHouseUnitTest/UnitTest1.cs
@@ -0,0 +1,59 @@
+using System;
+using NUnit.Framework;
+using DarkHouse.Inventory;
+using DarkHouse.Inventory.HouseHoldItem;
+
+namespace DarkHouseUnitTest
+{
+    public class Tests
+    {
+        private Refrigerator rf;
+
+        [SetUp]
+        public void Setup()
+        {
+            rf = new Refrigerator("Sharp 1", "Refrigerator 1", "Sharp Refrigerator no. 1");
+            rf.Close();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            if (!rf.IsOpen)
+            {
+                if (rf.Open())
+                {
+                    Assert.Pass($"Successfully opened {rf.Name}'s door.");
+                }
+                else
+                {
+                    Assert.Fail($"Fail to open {rf.Name}.");
+                }    
+            }
+            else
+            {
+                Assert.Pass($"{rf.Name}'s door is already opened.");
+            }
+        }
+
+        [Test]
+        public void Test2()
+        {
+            if (rf.IsOpen)
+            {
+                if (rf.Close())
+                {
+                    Assert.Pass($"Successfully closed {rf.Name}'s door.");
+                }
+                else
+                {
+                    Assert.Fail($"Fail to close {rf.Name}.");
+                }
+            }
+            else
+            {
+                Assert.Pass($"{rf.Name}'s door is already closed.");
+            }
+        }
+    }
+}
